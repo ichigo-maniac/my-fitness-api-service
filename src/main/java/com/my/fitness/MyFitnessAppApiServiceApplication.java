e799e71c89e0d0ab6eb8c4486dd2e630760f98fb
Dmitry Konoplev <dmitry.konoplev@citeck.ru>
2018-01-16 12:43:06 +0300

    Initial commit

---
+++ src/main/java/com/my/fitness/MyFitnessAppApiServiceApplication.java
@@ -0,0 +1,14 @@
+package com.my.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * My fitness API application
+ */
+@SpringBootApplication
+public class MyFitnessAppApiServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitnessAppApiServiceApplication.class, args);
+	}
+}
