768c035d744da80baa46ecfb1143384e43b44c9c
impi <ichigomaniac@gmail.com>
2018-01-23 19:07:41 +0300

    Added H2 Test Datasource configuration

---
+++ src/main/java/com/my/fitness/constants/MyFitnessApplicationConstants.java
@@ -0,0 +1,14 @@
+package com.my.fitness.constants;
+
+/**
+ * My fitness application constants
+ */
+public class MyFitnessApplicationConstants {
+
+    /**
+     * Profiles constants
+     */
+    public class PROFILES {
+        public static final String TEST_PROFILE = "test_profile";
+    }
+}
--- src/test/java/com/my/fitness/MyFitnessAppApiServiceApplicationTests.java
+++ src/test/java/com/my/fitness/MyFitnessAppApiServiceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.my.fitness;
 
+import com.my.fitness.configuration.TestDatabaseConnectionConfiguration;
+import com.my.fitness.constants.MyFitnessApplicationConstants;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {TestDatabaseConnectionConfiguration.class})
 @SpringBootTest
-public class MyFitnessAppApiServiceApplicationTests {
+@ActiveProfiles(MyFitnessApplicationConstants.PROFILES.TEST_PROFILE)
+public class MyFitnessAppApiServiceApplicationTests extends Assert {
 
 	@Test
 	public void contextLoads() {
