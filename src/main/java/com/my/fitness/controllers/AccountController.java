455d834e076e62eec43ca345b60ce4e7fd113a59
impi <ichigomaniac@gmail.com>
2018-03-03 12:14:31 +0300

    Added getting account by uuid

--- src/main/java/com/my/fitness/controllers/AccountController.java
+++ src/main/java/com/my/fitness/controllers/AccountController.java
@@ -2,16 +2,12 @@ package com.my.fitness.controllers;
 
 import com.my.fitness.controllers.dto.CreateAccountRequest;
 import com.my.fitness.converters.impl.AccountEntityConverter;
+import com.my.fitness.dto.AccountEntityDto;
 import com.my.fitness.entities.AccountEntity;
 import com.my.fitness.enums.SocialNetworkType;
 import com.my.fitness.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Account controller
@@ -33,12 +29,23 @@ public class AccountController {
     private AccountEntityConverter accountEntityConverter;
 
     /**
+     * Get account by uuid
+     * @param accountUUID Account uuid
+     * @return Account
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/get_account/{accountUUID}")
+    public AccountEntityDto getAccountByUUID(@PathVariable String accountUUID) {
+        AccountEntity account = accountService.getAccountByUUID(accountUUID);
+        return accountEntityConverter.convert(account);
+    }
+
+    /**
      * Get or create an account
      * @param createAccountRequest Create account request
      * @return Account info
      */
     @RequestMapping(method = RequestMethod.POST, value = "/get_or_create_account")
-    public Object getOrCreateAccount(@RequestBody CreateAccountRequest createAccountRequest) {
+    public AccountEntityDto getOrCreateAccount(@RequestBody CreateAccountRequest createAccountRequest) {
         AccountEntity account = accountService.getOrCreateAccount(createAccountRequest.getSocialNetworkId(),
                 SocialNetworkType.valueOf(createAccountRequest.getSocialNetworkType()),
                 createAccountRequest.getName());
